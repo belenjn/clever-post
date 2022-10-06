2af379971558fc52568d18c7ef2babe8dbeeea64
Belen JN <belenjarabanavas31@gmail.com>
2022-10-06 16:43:39 +0200

    Navbar and responsive done

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,11 @@
 export const Navbar = () => {
   return (
     <div className="navbar">
+      <div className="navbar__icon--container">
         <h1>CLEVER-POST</h1>
+        <div className="navbar__image" />
+      </div>
+      <div className="navbar__hamburger"/>
     </div>
-  )
-}
+  );
+};
