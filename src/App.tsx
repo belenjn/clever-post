aff157e8a9b4c63eb18a8ceece5984bab1884b5b
Belen JN <belenjarabanavas31@gmail.com>
2022-10-06 16:00:23 +0200

    Added some images in assets and styles in App.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import { Navbar } from "./components/Navbar";
 import "./styles/App.scss"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+        <h1>CLEVER-POST</h1>
+    </div>
+  )
+}
