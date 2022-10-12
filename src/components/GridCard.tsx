26dab02ddc4ef49619a12d54250d146a4e2ade27
Belen JN <belenjarabanavas31@gmail.com>
2022-10-12 14:27:07 +0200

    Deleted usersImage[]

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,29 +1,33 @@
 import React, { useState } from "react";
 import { Post } from "../features/postsSlice";
 import { ShowGridCard } from "./ShowGridCard";
-import { usersImages } from "../assets/usersImages";
 
 export const GridCard = ({ post }: { post: Post }) => {
   const [editPost, setEditPost] = useState(false);
 
+  const idPhoto: number = post.id;
 
-  const handleClickEdit = () => {
-    !editPost ? <ShowGridCard post={post} /> : "";
-  };
+  // const handleClickEdit = () => {
+  //   !editPost ? <ShowGridCard post={post} /> : "";
+  // };
 
   return (
     <>
       <div className="grid__card">
         <div className="grid__card--title-image">
           <h3>{post.title}</h3>
-          <img className="grid__card--image" src={usersImages[post.id]} />
+          <img
+            className="grid__card--image"
+            src={`https://i.pravatar.cc/150?u=${idPhoto}`}
+            alt="User icon"
+          />
         </div>
         <p>{post.body}</p>
 
         <div className="grid__card--icons-container">
           <div
             className="grid__card--icons-container-edit"
-            onClick={handleClickEdit}
+            // onClick={handleClickEdit}
           />
           <div className="grid__card--icons-container-delete" />
         </div>
