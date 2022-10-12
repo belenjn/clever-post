ec3feafe73ce47a5de4c5847f081e8a16329c69f
Belen JN <belenjarabanavas31@gmail.com>
2022-10-12 12:55:43 +0200

    Delete numFunc in GridCard bc had different photo

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,31 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Post } from "../features/postsSlice";
 import { ShowGridCard } from "./ShowGridCard";
 import { usersImages } from "../assets/usersImages";
 
 export const GridCard = ({ post }: { post: Post }) => {
-  
-  function getRandomNumber(max: number): number {
-    return Math.floor(Math.random() * max);
-  }
+  const [editPost, setEditPost] = useState(false);
 
-  // const handleClickEdit = () => {
-  // }
+
+  const handleClickEdit = () => {
+    !editPost ? <ShowGridCard post={post} /> : "";
+  };
 
   return (
     <>
       <div className="grid__card">
         <div className="grid__card--title-image">
           <h3>{post.title}</h3>
-          <img
-            className="grid__card--image"
-            src={usersImages[getRandomNumber(10)]}
-          />
+          <img className="grid__card--image" src={usersImages[post.id]} />
         </div>
         <p>{post.body}</p>
 
         <div className="grid__card--icons-container">
-          <div className="grid__card--icons-container-edit" />
+          <div
+            className="grid__card--icons-container-edit"
+            onClick={handleClickEdit}
+          />
           <div className="grid__card--icons-container-delete" />
         </div>
       </div>
--- src/components/ShowGridCard.tsx
+++ src/components/ShowGridCard.tsx
@@ -1,20 +1,14 @@
-export const ShowGridCard = () => {
+import { Post } from "../features/postsSlice";
+
+export const ShowGridCard = ({ post }: { post: Post }) => {
   return (
     <div className="showGridCard__background">
       <div className="showGridCard__container">
         <div className="showGridCard__container--title-image">
-          <h3>
-            Sunt aut facere repellat provident occaecati excepturi optio
-            reprehenderit
-          </h3>
+          <h3>{post.title}</h3>
           <div className="showGridCard__container--image" />
         </div>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only.
-        </p>
+        <p>{post.body}</p>
 
         <button className="showGridCard__container--button">Save</button>
       </div>
