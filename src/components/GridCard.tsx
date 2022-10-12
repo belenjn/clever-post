4b7de33b33cca52d39ae1d0809b6b19a0af2c9a2
Belen JN <belenjarabanavas31@gmail.com>
2022-10-12 12:41:32 +0200

    Photo to GridCard added

--- src/components/GridCard.tsx
+++ src/components/GridCard.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Post } from "../features/postsSlice";
 import { ShowGridCard } from "./ShowGridCard";
+import { usersImages } from "../assets/usersImages";
 
 export const GridCard = ({ post }: { post: Post }) => {
+  
+  function getRandomNumber(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
+
   // const handleClickEdit = () => {
   // }
 
@@ -11,7 +17,10 @@ export const GridCard = ({ post }: { post: Post }) => {
       <div className="grid__card">
         <div className="grid__card--title-image">
           <h3>{post.title}</h3>
-          <div className="grid__card--image" />
+          <img
+            className="grid__card--image"
+            src={usersImages[getRandomNumber(10)]}
+          />
         </div>
         <p>{post.body}</p>
 
